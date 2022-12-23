770179d32741e746e5b41f6e5bf63ae09d3fb32f
Surya Negara <21523263@students.uii.ac.id>
2022-12-23 19:07:56 +0700

    feat: init timer machine & session machine

--- src/Machines/SessionMachine/index.ts
+++ src/Machines/SessionMachine/index.ts
@@ -1,1 +1,149 @@
-export default {}
+import {
+    ActorRefWithDeprecatedState,
+    assign,
+    send,
+    spawn,
+    actions,
+    createMachine,
+} from 'xstate'
+import TimerMachine from '../TimerMachine'
+import { TimerEventStart, TimerMachineContext, TimerMachineEvent, TimerMachineEventType } from '../TimerMachine/types'
+
+export type WithStartAndEndDate = {
+    startDate: Date
+    endDate: Date
+}
+
+export type Session = {
+    type: "session" | "rest",
+    title: string
+    note: string
+    projectId: string
+    pauses: WithStartAndEndDate[],
+} & WithStartAndEndDate;
+
+export type SessionMachineContext = {
+    timerRef?: ActorRefWithDeprecatedState<
+        TimerMachineContext,
+        TimerMachineEvent,
+        any,
+        any
+    >
+    sessions: Session[]
+    startDate: Date
+    pauseDate: Date
+    pauses: WithStartAndEndDate[]
+    title: string
+}
+
+type SessionMachineEvent =
+  | {
+      type: "INIT_FOCUS";
+      second: number;
+      startDate: Date;
+    }
+  | {
+      type: "BREATHE_END";
+    }
+  | {
+      type: "TIMER_END";
+    }
+  | { type: "OVERFLOW_REVERSED" }
+  | {
+      type: "STOP_WORKING";
+    }
+  | {
+      type: "COMPLETE_TASK";
+    }
+  | { type: "PAUSE" }
+  | { type: "UNPAUSE" }
+  | { type: "INCREMENT"; second: number }
+  | { type: "DECREMENT"; second: number }
+  | { type: "ABANDON_SESSION" }
+  | { type: "OVERRIDE_STATE"; state: Partial<SessionMachineContext> }
+  | ({
+      type: "RECORD_PAUSE";
+      nextTarget?: "idle" | "rest";
+    } & WithStartAndEndDate)
+  | { type: "TAKE_A_BREAK"; second: number };
+
+
+const sessionMachineInitialState: SessionMachineContext = {
+    timerRef: undefined,
+    startDate: new Date(),
+    pauseDate: new Date(),
+    pauses: [],
+    title: "",
+    sessions: []
+}
+
+export enum SessionMachineEventType {
+    PAUSE = "PAUSE",
+}
+
+export type SessionEventPause = {
+    type: SessionMachineEventType.PAUSE
+    startDate: Date
+    endDate: Date
+}
+
+const appendPauses = assign<
+    SessionMachineContext,
+    SessionEventPause
+>((ctx, event) => ({
+    pauses: [
+        ...ctx.pauses,
+        {
+            startDate: event.startDate,
+            endDate: event.endDate
+        }
+    ],
+}))
+
+const spawnTimer = [
+    assign<SessionMachineContext>((ctx) => ({
+        timerRef: spawn(TimerMachine, "timer"),
+        startDate: new Date()
+    })),
+    send<TimerMachineContext,TimerEventStart>(
+        (_,ev) => ({
+            type: TimerMachineEventType.START,
+            durationSecond: ev.durationSecond ?? 999
+        })
+    ),
+    {
+        to: "timer"
+    }
+]
+
+const commitTimer = (type: "session" | "rest" ) =>
+    assign<SessionMachineContext>((ctx) => ({
+        sessions: [
+            ...ctx.sessions,
+            {
+                type,
+                title: ctx.title,
+                startDate: ctx.startDate,
+                endDate: new Date(),
+                note: "",
+                projectId: "",
+                pauses: ctx.pauses
+            }
+        ]
+    }))
+
+const resetTimerActions = [
+    actions.stop("timer"),
+    assign<SessionMachineContext>({
+        timerRef: undefined,
+        title: "",
+        pauses: []
+    })
+]
+
+const SessionMachine = createMachine<
+    SessionMachineContext,
+    SessionMachineEvent
+>({},{})
+
+export default SessionMachine;
--- src/Machines/TimerMachine/index.ts
+++ src/Machines/TimerMachine/index.ts
@@ -1,1 +1,175 @@
-export default {}
+import {
+    createMachine,
+    assign,
+    send,
+    sendParent,
+} from 'xstate'
+import {
+    TimerMachineContext,
+    TimerMachineEventType,
+    TimerMachineEvent,
+    TimerMachineStates,
+    TimerEventStart,
+    TimerEventTick
+} from './types'
+
+const timerMachineInitialState: TimerMachineContext = {
+    startDate: new Date,
+    durationSecond: 0,
+    secondRemaining: 0,
+    pauseBuffer: 0,
+    pauseDate: new Date()
+}
+
+const resetToInitialState = assign(() => timerMachineInitialState)
+
+const startSession = assign<TimerMachineContext, TimerEventStart>((_, event) => ({
+    startDate: new Date(),
+    durationSecond: event.durationSecond,
+    secondRemaining: event.durationSecond
+}))
+
+const setSecondRemaining = assign<TimerMachineContext, TimerEventTick>((context) => {
+    let relativeSecondElapsed =
+        (Date.now() - context.startDate.getTime() - context.pauseBuffer * 1000) /
+        1000;
+    let secondRemaining = context.durationSecond - relativeSecondElapsed;
+    return {
+        secondRemaining: Number(secondRemaining.toFixed(0))
+    }
+})
+
+const TimerMachine = createMachine<TimerMachineContext, TimerMachineEvent>(
+    {
+        predictableActionArguments: true,
+        id: "timer",
+        initial: TimerMachineStates.IDLE,
+        context: timerMachineInitialState,
+        invoke: {
+            src: () => send => {
+                let interval = setInterval(() => {
+                    send(TimerMachineEventType.TICK);
+                  }, 1000);
+                  return () => clearInterval(interval);
+            }
+        },
+        states: {
+            [TimerMachineStates.IDLE]: {
+                entry: resetToInitialState,
+                on: {
+                    [TimerMachineEventType.START]: {
+                        target: TimerMachineStates.RUNNING,
+                        actions: startSession
+                    }
+                }
+            },
+            [TimerMachineStates.RUNNING]: {
+                on: {
+                    [TimerMachineEventType.PAUSE]: {
+                        target: TimerMachineStates.PAUSED
+                    }
+                },
+                always: [
+                    {
+                        target: TimerMachineStates.OVERFLOWED,
+                        cond: (ctx,ev) => ctx.secondRemaining < 0
+                    }
+                ]
+            },
+            [TimerMachineStates.OVERFLOWED]: {
+                entry: sendParent("TIMER_END"),
+                always: [
+                    {
+                        target: TimerMachineStates.RUNNING,
+                        cond: (ctx) => ctx.secondRemaining >= 0,
+                        actions: sendParent("OVERFLOW_REVERSED")
+                    }
+                ],
+                on: {
+                    [TimerMachineEventType.INCREMENT]: {
+                        actions: [
+                            assign((ctx,ev) => ({
+                                durationSecond: 
+                                    (Date.now() - ctx.startDate.getTime()) / 1000 + ev.second
+                            })),
+                            send(TimerMachineEventType.TICK)
+                        ]
+                    },
+                    [TimerMachineEventType.PAUSE]: {
+                        target: TimerMachineStates.PAUSED
+                    },
+                }
+            },
+            [TimerMachineStates.PAUSED]: {
+                entry: [
+                    assign((ctx, ev) => ({
+                        pauseDate: new Date()
+                    })),
+                    sendParent({
+                        type: "OVERRIDE_STATE",
+                        state: { pauseDate: new Date() }
+                    })
+                ],
+                on: {
+                    [TimerMachineEventType.PAUSE]: undefined,
+                    [TimerMachineEventType.TICK]: {
+                        actions: setSecondRemaining
+                    },
+                    [TimerMachineEventType.UNPAUSE]: {
+                        target: TimerMachineStates.RUNNING,
+                        actions: [
+                            assign((ctx, ev) => ({
+                                pauseBuffer:
+                                  ctx.pauseBuffer +
+                                  (Date.now() - ctx.pauseDate.getTime()) / 1000
+                              })),
+                              sendParent((ctx, ev) => ({
+                                ...ev,
+                                type: "RECORD_PAUSE",
+                                startDate: ctx.pauseDate,
+                                endDate: new Date()
+                            }))
+                        ]
+                    }
+                }
+            },
+        },
+        on: {
+            [TimerMachineEventType.STOP]: {
+                target: TimerMachineStates.IDLE
+            },
+            [TimerMachineEventType.TICK]: {
+                actions: assign((context) => {
+                    let relativeSecondElapsed =
+                        (Date.now() - context.startDate.getTime() - context.pauseBuffer * 1000) /
+                        1000;
+                    let secondRemaining = context.durationSecond - relativeSecondElapsed;
+                    return {
+                        secondRemaining: Number(secondRemaining.toFixed(0))
+                    }
+                })
+            },
+            [TimerMachineEventType.INCREMENT]: {
+                actions: [
+                    assign((ctx,ev) => ({
+                        durationSecond: ctx.durationSecond + ev.second
+                    })),
+                    send(TimerMachineEventType.TICK)
+                ]
+            },
+            [TimerMachineEventType.DECREMENT]: {
+                actions: [
+                    assign((ctx, ev) => ({
+                        durationSecond: Math.max(ctx.durationSecond - ev.second, 0)
+                      })),
+                      send(TimerMachineEventType.TICK)
+                ]
+            },
+            [TimerMachineEventType.OVERRIDE_STATE]: {
+                actions: () => console.log('test')
+            }
+        }
+    }
+)
+
+export default TimerMachine
